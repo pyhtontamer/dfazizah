797252e792db56a452d189bb17efec407489072a
PythonTamer <cakazizilham@gmail.com>
2024-09-21 17:37:26 +0700

    berubah hahaha

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ print("hello world guys")
 print ("hello dek")
 print("hahaha")
 print("hihiiihi")
+print("loloo")
+print("kokooihi")
+print("nanana")
+print("hkakakaka")
 
-#ini ada di cabang dev
+
+#ini ada di cabang dev akamsi
